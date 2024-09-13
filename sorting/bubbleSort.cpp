2731aa5828bf9f5fb412fe88309e7b8adcf7dfac
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2024-09-13 19:38:53 +0530

    ok

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,35 +1,32 @@
-#include<bits/stdc++.h>
-
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
 using namespace std;
-void selection_sort(int arr[], int n) {
-  // selection sort
-  for (int i = 0; i < n - 1; i++) {
-    int mini = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[mini]) {
-        mini = j;
+void selectionSort(int *arr,int n){
+      for(int i=0;i<n-1;i++){
+        int flag = i;
+        for(int j=i+1;j<n;j++){
+          if(arr[j]<arr[flag]){
+            flag = j;
+          }
+        }
+        swap(arr[i],arr[flag]); 
       }
-    }
-    int temp = arr[mini];
-    arr[mini] = arr[i];
-    arr[i] = temp;
-  }
-
-  cout << "After selection sort: " << "\n";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-
 }
-int main() {
-  int arr[] = {13,46,24,52,20,9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-   cout << "Before selection sort: " << "\n";
-   for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
+int main()
+{
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
   }
-  cout << "\n";
-  selection_sort(arr, n);
+  selectionSort(arr,n);
   return 0;
 }
